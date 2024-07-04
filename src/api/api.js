2d56d49731c332808fb5a7f75aea3a282b3e0984
Baskar M <baskarvhp98@gmail.com>
2024-07-04 23:32:32 +0530

    Hosting Changes

---
+++ src/api/api.js
@@ -0,0 +1,11 @@
+const apiKey = '211a599f2b086e5d7a4c4d3574a41b34';
+
+const getWeather = async (city) => {
+    return await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`)
+    .then((res) => res.json())
+    .then((json) => {
+        return json;
+    })
+}
+
+export default getWeather;
